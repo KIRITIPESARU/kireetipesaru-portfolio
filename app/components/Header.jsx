a5289d68827fcef304e78b57fb11127687be7be6
KIRITIPESARU <pesaruk13@gmail.com>
2026-09-26 17:05:13 +0530

    Update Header.jsx

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -5,15 +5,17 @@ import { assets } from '@/assets/assets'
 const Header = () => {
     return (
         <div className="w-11/12 max-w-3x1 text-center mx-auto h-screen flex flex-col justify-center items-center gap-4">
-            <div>
-                <Image src={assets.profile_img} alt="Profile" className="w-32 rounded-full" />
-            </div>
-            <h3 className="flex items-end text-xl md:text-2xl mb-3 font-ovo">
+            <div><Image src={assets.profile_img} alt="Profile" className="w-32 rounded-full" /></div>
+            {/* <h3 className="flex items-end text-xl md:text-2xl mb-3 font-ovo">
+                Hi! I'm Kireeti Pesaru <Image src={assets.hand_icon} alt="Hand_Icon" className="w-6 h-6" />
+            </h3> */}
+            <h3 className="flex items-center justify-center gap-2 mb-2 text-2xl font-Ovo">
                 Hi! I'm Kireeti Pesaru <Image src={assets.hand_icon} alt="Hand_Icon" className="w-6 h-6" />
             </h3>
-            <h1 className="text-3xl sm:text-6xl lg:text-[62px] font-ovo font-bold">
+            {/* <h1 className="text-3xl sm:text-6xl lg:text-[62px] font-ovo font-bold">
                 Frontend Web Developer | React.js Developer
-            </h1>
+            </h1> */}
+            <h1 className="text-center mb-2 text-5xl font-Ovo">Frontend Web Developer</h1>
             <p className="max-w-2x1 mx-auto font-Ovo">
                 Frontend Developer with 1.6 years of hands-on experience building responsive and user-friendly web applications using React.js, JavaScript, 
                 HTML5, CSS3, and Tailwind CSS. Skilled in REST API integration, UI development, debugging, and application support, with a strong foundation 
